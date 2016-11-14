4c17833e1b1d473c666562f43ece9708cb74a597
ivanjohnting <ivanjohnting@gmail.com>
2016-11-14 11:36:21 +0800

    Update Part2.py

--- Part2.py
+++ Part2.py
@@ -2,7 +2,7 @@ import re
 from collections import Counter
 from collections import namedtuple
 
-train = open('train', encoding = "utf8")
+train = open('EN/train', encoding = "utf8")
 
 train_xlist=[]
 train_ylist=[]
@@ -18,23 +18,41 @@ train_xcount = Counter(train_xlist)
 train_ycount = Counter(train_ylist)
 train_xycount = Counter(tuple(i) for i in train_xylist)
 # print (train_xcount)
-# print (train_ycount)
+print (train_ycount)
 # print(train_xycount)
 
 emissiondict = {}
 for key in train_xycount:
 	emission = train_xycount[key]/train_ycount[key[1]]
 	emissiondict[key] = emission
-#print (emissiondict)
+# print (emissiondict)
 
-test = open('dev.in', encoding = "utf8")
+test = open('EN/dev.in', encoding = "utf8")
 
-# for lines in test:
-# 	line=lines.split()
-#	print (line)
-# 	if line!=[]:
-# 		if line[0] not in train_xcount:
-# 			emission = 1/
+for words in test: 	
+  	x=words.split()
+  	if line!=[]:
+  		if line[0] not in train_xcount:
+  			for y in train_ycount:
+  				emission = 1/(train_ycount[y]+1)
+  				train_ycount[y] += 1
+  				emissiondict[(x,y)] = emission
+  		if line[0] in train_xcount:
+  			
+
+#print emissiondict[('ivan','testing')]
 
 
+for y in train_ycount:
+	train_ycount[y]+=1
 
+print (train_ycount)
+
+# a={(',','O'):424}
+
+# print (a[(',','O')])
+# for k1,k2 in a:
+# 	print (k2)
+
+#a=('1','2')
+#print (a[0])
